3fb8dd7e775560adc49db048d419243e7299a565
Raza Mohamed <mnrazamohamed@gmail.com>
2022-07-31 13:00:39 +0530

    removed models from app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 require("dotenv/config");
 require("./config/dbConfig");
 require("./app/model/user/user");
-// require("./app/model/post/post");
-// require("./app/model/boarding/boarding");
 const notFound = require("./app/middleware/notfound");
 
 // Middleware
@@ -18,17 +16,15 @@ app.use(
 app.use(express.json());
 app.use(express.static("./assets"));
 
-
-
 //import routes
 const user = require("./app/router/user");
 const post = require("./app/router/post");
 const boarding = require("./app/router/boarding");
 
 //routes
-app.use("/api/v1/user", user); 
-app.use("/api/v1/post", post); 
-app.use("/api/v1/boarding", boarding); 
+app.use("/api/v1/user", user);
+app.use("/api/v1/post", post);
+app.use("/api/v1/boarding", boarding);
 app.use(notFound);
 
 const port = process.env.PORT || 5000;
