a824532a4c3837e6c02097e9d2e986ac775b4e68
Raza Mohamed <83370897+mnrazamohamed@users.noreply.github.com>
2022-08-03 01:28:30 +0530

    Update payment.js

--- model/payment.js
+++ model/payment.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = mongoose.Schema(
-  {
+const paymentSchema = mongoose.Schema({
     userID: {
       type: String,
       required: [true, "Please provide UserID"],
-      minLength: 3,
-      maxLength: 50,
+      trim: true,
+    },
+    postID: {
+      type: String,
+      required: false,
       trim: true,
     },
     amount: {
       type: Number,
       required: [true, "Please provide amount in LKR"],
     },
-  },
-  {
+  },{
     timestamps: true,
   }
 );
