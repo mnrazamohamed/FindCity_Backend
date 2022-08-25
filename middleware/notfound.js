5bbfd186acc54c453f7232ec1fb7db4418107e7e
Raza Mohamed <mnrazamohamed@gmail.com>
2022-08-26 01:08:40 +0530

    not found

--- middleware/notfound.js
+++ middleware/notfound.js
@@ -1,10 +1,11 @@
 const { StatusCodes } = require("http-status-codes")
 
 const notfound = (req, res) => {
-    res.status(StatusCodes.NOT_FOUND).json( {
-        status : StatusCodes.NOT_FOUND,
-        data : "Invalid path"
-    })
+    // res.status(StatusCodes.NOT_FOUND).json( {
+    //     status : StatusCodes.NOT_FOUND,
+    //     data : "Invalid path"
+    // })
+    res.send("<h1>Welcome</h1>")
 }
 
 module.exports = notfound
